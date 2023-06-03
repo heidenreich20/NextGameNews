6c6070c5fd38d5ddd328cd459167bb61e20209c3
heidenreich20 <heidenreich20@hotmail.com>
2023-06-03 01:00:37 -0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,16 @@
 'use client'
 import './globals.css'
+import { ReactNode } from 'react'
 import { Header, Navbar } from './components'
 import { DarkModeProvider } from './context/DarkModeContext'
 import { NewsListProvider } from './context/NewsListContext'
 
-export default function NavigationLayout ({ children }) {
+type Props = {
+  children: ReactNode;
+};
+
+
+export default function NavigationLayout ({ children }: Props) {
   return (
     <html>
       <body> 
