e4a2ef963130ff4b41bebad1f7b64ecc5459fe97
heidenreich20 <heidenreich20@hotmail.com>
2023-06-12 18:33:45 -0300

    Update page.jsx

--- app/analisis/[postId]/page.jsx
+++ app/analisis/[postId]/page.jsx
@@ -1,10 +1,7 @@
 import ArticleReviewBody from '../../components/ArticleReviewBody'
-import { Suspense } from 'react'
 
 export default function Analisis () {
   return (
-    <Suspense fallback={<div>Hello world</div>}>
-      <ArticleReviewBody />
-    </Suspense>
+    <ArticleReviewBody />
   )
 }
