ed1015c601256f2e8f64c63b56285d2afce392d8
heidenreich20 <heidenreich20@hotmail.com>
2023-06-17 15:38:57 -0300

    Update Header.jsx

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -10,18 +10,13 @@ const Header = () => {
   const [openMenu, setOpenMenu] = useState(false)
 
   return (
-    <header className='gradient-banner border-b-2 border-b-slate-700 sticky top-0 z-50 m-auto flex sm:justify-center justify-between h-14 place-items-center items-center gap-1 csm:static csm:gap-4 md:h-20'>
-      <IconButton
-        className='sm:hidden flex'
+    <header className='gradient-banner border-b-2 border-b-slate-700 sticky top-0 z-50 m-auto flex sm:grid content-center sm:justify-center justify-between h-14 place-items-center items-center gap-1 csm:static csm:gap-4 md:h-20'>
+      <button
+        className='flex sm:hidden p-1'
         onClick={() => setOpenMenu(true)}
-        sx={{
-          borderRadius: '1rem',
-          fontSize: '1.2rem',
-          fontFamily: 'var(--main-font)'
-        }}
       >
-        <MenuIcon sx={{ fontSize: '2rem' }} />
-      </IconButton>
+        <MenuIcon sx={{ fontSize: '3rem' }} />
+      </button>
       <DrawerComponent
         onOpen={openMenu}
         open={openMenu}
@@ -34,7 +29,7 @@ const Header = () => {
       >
         <h1>GAMENEWS</h1>
       </Link>
-      <section className='block md:hidden'>
+      <section className='block sm:hidden'>
         <SearchBar />
       </section>
     </header>
